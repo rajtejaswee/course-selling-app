3f59674dbb7ee01693b257b79bfec31f43b62502
Raj Tejaswee <rajtejaswee02@gmail.com>
2025-12-10 00:27:06 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,19 @@
+import express from 'express'
+const app = express()
+
+app.post('/user/login', (req,res) => {
+    
+})
+app.post('/user/signin', (req,res) => {
+
+})
+app.post('/user/purchase-courses', (req,res) => {
+
+})
+app.post('/user/purchased', (req,res) => {
+
+})
+
+
+
+app.listen(8000);
